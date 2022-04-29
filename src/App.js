9c37aa78aa7c3a36cdda772ca42a17d72650487f
Pedro Alquimim <pedrohalquimim@gmail.com>
2022-04-29 17:30:40 -0300

    .

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
+import { ExpenseItem } from './components/ExpenseItem';
+
 function App() {
   return (
-    <h1>Hello World</h1>
+    <ExpenseItem></ExpenseItem>
   );
 }
 
---
+++ src/components/ExpenseItem.jsx
@@ -0,0 +1,11 @@
+export function ExpenseItem() {
+  return (
+    <div>
+      <div>March 28th 2021</div>
+      <div>
+        <h2>Car Insurance</h2>
+        <div>$294.67</div>
+      </div>
+    </div>
+  );
+}
