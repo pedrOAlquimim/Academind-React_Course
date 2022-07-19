d60b367161e82debf82f39274cd81e48442bf2f8
Pedro Alquimim <pedrohalquimim@gmail.com>
2022-07-19 17:24:24 -0300

    .

--- src/components/Expenses/ExpenseItem.jsx
+++ src/components/Expenses/ExpenseItem.jsx
@@ -1,10 +1,10 @@
-import './ExpenseItem.css';
-
 import { ExpenseDate } from './ExpenseDate'
+import { Card } from '../UI/Card';
+import './ExpenseItem.css';
 
 export function ExpenseItem(props) {
   return (
-    <div className='expense-item'>
+    <Card className='expense-item'>
       <ExpenseDate 
         date={props.date}
       />
@@ -12,6 +12,6 @@ export function ExpenseItem(props) {
         <h2>{props.title}</h2>
         <div className='expense-item__price'>${props.amount}</div>
       </div>
-    </div>
+    </Card>
   );
 }
--- src/components/Expenses/Expenses.jsx
+++ src/components/Expenses/Expenses.jsx
@@ -1,10 +1,10 @@
-import './Expenses.css'
-
 import { ExpenseItem } from './ExpenseItem';
+import { Card } from '../UI/Card';
+import './Expenses.css'
 
 export function Expenses(props) {
   return (
-    <div className='expenses'>
+    <Card className='expenses'>
       <ExpenseItem
         title={props.items[0].title}
         amount={props.items[0].amount}
@@ -25,6 +25,6 @@ export function Expenses(props) {
         amount={props.items[3].amount}
         date={props.items[3].date}
       />
-    </div>
+    </Card>
   );
 }
